ee48b9dea4f8cc6cad5dcb40f10d7eabd08f93b5
Henry Unite <unitehenry@gmail.com>
2026-04-09 04:50:10 +0000

    refactor: argv

--- src/main.c
+++ src/main.c
@@ -120,7 +120,7 @@ void generate_pages(StringList *list) {
   }
 }
 
-int main(int argc, char **args) {
+int main(int argc, char **argv) {
   if (argc < 2) {
     fprintf(stderr, "Must provide content directory\n");
 
@@ -129,7 +129,7 @@ int main(int argc, char **args) {
 
   StringList *list = init_string_list(10); // start with capacity 10
 
-  get_content_files(list, args[1]);
+  get_content_files(list, argv[1]);
 
   generate_pages(list);
 
