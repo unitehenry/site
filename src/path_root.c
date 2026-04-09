5ed012a829b67428e9eea8db141539d2e9bc95fe
Henry Unite <unitehenry@gmail.com>
2026-04-09 06:00:02 +0000

    feat: path root

--- src/path_root.c
+++ src/path_root.c
@@ -2,14 +2,37 @@
 #include <libgen.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 
 void path_root(char **root, char *path) {
   char *path_copy = strdup(path);
+
   if (!path_copy) {
     *root = NULL;
     return;
   }
+
   char *dir = dirname(path_copy);
-  *root = strdup(dir);
+
+  char *last_dir = malloc(strlen(dir) + 1);
+
+  strcpy(last_dir, dir);
+
+  while (strcmp(dir, ".") != 0) {
+    dir = dirname(path_copy);
+
+    if (strcmp(dir, ".") != 0) {
+      free(last_dir);
+
+      last_dir = malloc(strlen(dir) + 1);
+
+      strcpy(last_dir, dir);
+    }
+  }
+
+  *root = strdup(last_dir);
+
+  free(last_dir);
+
   free(path_copy);
 }
